91641de31735d5eb6bea6306c1e9d794263ec15e
kris <kriskuerten@gmail.com>
2026-02-14 03:29:19 -0800

    node

---
+++ Assets/Scripts/NodeSlot.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class NodeSlot : MonoBehaviour
+{
+    public List<NodeSlot> nearbyNodes = new List<NodeSlot>();
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
