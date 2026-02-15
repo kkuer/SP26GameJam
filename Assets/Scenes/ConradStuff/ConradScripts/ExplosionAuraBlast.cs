21fa3feabfb1e042ba5fb122e84c5008de09c083
Mason <114711416+CMillikn@users.noreply.github.com>
2026-02-14 22:11:22 -0800

    Weapons Drop

--- Assets/Scenes/ConradStuff/ConradScripts/ExplosionAuraBlast.cs
+++ Assets/Scenes/ConradStuff/ConradScripts/ExplosionAuraBlast.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class ExplosionAuraBlast : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject bulletSplash;
+    public EnemyTag enemyTag;
+    public float auraGrowSpeed;
+    public float auraGrowMaxSize;
+
+    public void Start()
     {
-        
+        this.transform.localScale = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (this.transform.localScale.x < auraGrowMaxSize)
+        {
+            this.transform.localScale = new Vector2(this.transform.localScale.x + (auraGrowSpeed * Time.deltaTime), this.transform.localScale.y + (auraGrowSpeed * Time.deltaTime));
+        }
+        else if (this.transform.localScale.x > auraGrowMaxSize)
+        {
+            this.transform.localScale = new Vector2(auraGrowMaxSize, auraGrowMaxSize);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        enemyTag = col.gameObject.GetComponent<EnemyTag>();
+        if (enemyTag != null)
+        {
+            Instantiate(bulletSplash, col.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        enemyTag = col.gameObject.GetComponent<EnemyTag>();
+        if (enemyTag != null)
+        {
+            Instantiate(bulletSplash, col.transform.position, Quaternion.identity);
+        }
     }
 }
