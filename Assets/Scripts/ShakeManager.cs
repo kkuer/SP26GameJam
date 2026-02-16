309fca425ed1af65ecd4be7a5cf8cd7bc85c130f
kris <kriskuerten@gmail.com>
2026-02-15 18:34:29 -0800

    camera prefab
    ShakeManager.Instance.shakeCam(1f, 2f, 0.3f);

--- Assets/Scripts/ShakeManager.cs
+++ Assets/Scripts/ShakeManager.cs
@@ -53,10 +53,5 @@ public class ShakeManager : MonoBehaviour
                 perlin.FrequencyGain = 0f;
             }
         }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            shakeCam(3f, 2f, 0.3f);
-        }
     }
 }
